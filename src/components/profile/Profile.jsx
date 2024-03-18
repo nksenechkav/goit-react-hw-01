084919cd05f5eba017f0ab6c64c51367fab1bc9e
nksenechkav <144259943+nksenechkav@users.noreply.github.com>
2024-03-18 13:11:52 +0100

    add fix in Profile

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -2,26 +2,26 @@ import css from "./Profile.module.scss";
 
 const Profile = ({ name, tag, location, image, stats }) => {
   return (
-    <div className={css["profile-container"]}>
-      <div className={css["user-info"]}>
+    <div className={css["profile-card-container"]}>
+      <div className={css["user-card"]}>
         <img src={image} alt="User avatar" width="250" />
-        <p className={css["user-name"]}>{name}</p>
+        <p className={css["user-card-name"]}>{name}</p>
         <p>@{tag}</p>
         <p>{location}</p>
       </div>
 
-      <ul className={css["profile-stats"]}>
-        <li>
-          <span className={css["profile-stats-descr"]}>Followers</span>
-          <span className={css["profile-stats-number"]}>{stats.followers}</span>
+      <ul className={css["profile-info"]}>
+        <li className={css["profile-info-item"]}>
+          <span className={css["profile-info-name"]}>Followers</span>
+          <span className={css["profile-info-amount"]}>{stats.followers}</span>
         </li>
-        <li>
-          <span className={css["profile-stats-descr"]}>Views</span>
-          <span className={css["profile-stats-number"]}>{stats.views}</span>
+        <li className={css["profile-info-item"]}>
+          <span className={css["profile-info-name"]}>Views</span>
+          <span className={css["profile-info-amount"]}>{stats.views}</span>
         </li>
-        <li>
-          <span className={css["profile-stats-descr"]}>Likes</span>
-          <span className={css["profile-stats-number"]}>{stats.likes}</span>
+        <li className={css["profile-info-item"]}>
+          <span className={css["profile-info-name"]}>Likes</span>
+          <span className={css["profile-info-amount"]}>{stats.likes}</span>
         </li>
       </ul>
     </div>

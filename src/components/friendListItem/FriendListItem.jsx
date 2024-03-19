5e33407c5e268c6b1726059686f9251256ccbe3a
nksenechkav <144259943+nksenechkav@users.noreply.github.com>
2024-03-19 09:05:04 +0100

    add fix in FriendListItem

--- src/components/friendListItem/FriendListItem.jsx
+++ src/components/friendListItem/FriendListItem.jsx
@@ -3,10 +3,10 @@ import css from "./FriendListItem.module.scss";
 
 const FriendListItem = ({ avatar, name, isOnline }) => {
   return (
-    <div className={css["friend-list-item"]}>
-      <img className={css["friend-image"]} src={avatar} alt="Avatar" width="48" />
-      <p className={css["friend-name"]}>{name}</p>
-      <p className={clsx(css["friend-status"], isOnline && css["is-online"])}>
+    <div className={css.item}>
+      <img className={css.image} src={avatar} alt="Avatar" width="48" />
+      <p className={css.name}>{name}</p>
+      <p className={clsx(css.status, isOnline && css["is-online"])}>
         {isOnline ? "Online" : "Offline"}
       </p>
     </div>
